9ac6d76a25dd6a8f5688994eeb3fc8be15ff95c0
Sunjay Guttikonda <snguttikonda@cpp.edu>
2021-09-23 21:46:49 -0700

    Add total trip days and round trip airfare functions

--- functions.c
+++ functions.c
@@ -1,3 +1,16 @@
 #include<header.h>
 
 //Functions to be implemented Go here
+
+int total_trip_days() {
+    int num_of_days;
+    printf("Please enter the total number of days spent on the trip.\n");
+    scanf("%i", &num_of_days);
+    return num_of_days;
+}
+double round_trip_airfare(){
+    double round_trip_cost;
+    printf("What did a round-trip airfare cost?\n");
+    scanf("%lf", &round_trip_cost);
+    return round_trip_cost;
+}
--- header.h
+++ header.h
@@ -2,5 +2,7 @@
 #define HEADER_H
 
 //function declararations here
+int total_trip_days();
+double round_trip_airfare();
 
 #endif
