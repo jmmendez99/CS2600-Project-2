bfcdc46275ed8857a002be25b997a56416f42c04
Sunjay Guttikonda <snguttikonda@cpp.edu>
2021-09-25 15:02:40 -0700

    Header with days, airfare, vehicle, times declarations

--- header.h
+++ header.h
@@ -4,5 +4,6 @@
 //function declararations here
 int total_trip_days();
 double round_trip_airfare();
-
+double private_vehicle_cost(int miles_driven);
+int departure_arrival_times(double departure_time, double arrival_time);
 #endif
