bfeaf9875ef2d1dc361858dd16ea2e0b6e5f1949
Sunjay Guttikonda <snguttikonda@cpp.edu>
2021-09-25 19:25:26 -0700

    Modified function declarations: fixed function and return types

--- header.h
+++ header.h
@@ -4,6 +4,6 @@
 //function declararations here
 int total_trip_days();
 double round_trip_airfare();
-double private_vehicle_cost(int miles_driven);
-int departure_arrival_times(double departure_time, double arrival_time);
+double private_vehicle_cost(double miles_driven);
+double departure_arrival_times(double departure_time, double arrival_time);
 #endif
