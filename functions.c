f15c46612fb5318b168b8ce593b03a6c6e8d9053
TheAmitBarua <89492966+TheAmitBarua@users.noreply.github.com>
2021-09-26 17:43:01 -0700

    Update functions.c

--- functions.c
+++ functions.c
@@ -79,74 +79,74 @@ double parking_fees(int num_of_days){
 }
 
 double registration_fees()
-{
-   double reg_fee;
-   printf(" Enter registration fee \n");
-scanf(reg_fee);
-while(reg_fee<0)
-{
-printf(" Enter correct registration fee\n");
-scanf(reg_fee);
-}
-   totalExpenses=totalExpenses+reg_fee;
-return 0;
-}
+    {
+       double reg_fee;
+       printf(" Enter registration fee \n");
+    scanf(reg_fee);
+    while(reg_fee<0)
+    {
+    printf(" Enter correct registration fee\n");
+    scanf(reg_fee);
+    }
+       totalExpenses=totalExpenses+reg_fee;
+    return 0;
+    }
 
 double meals(int days, double arrival, double departure)
-{
-   int breakfast;
-   int lunch;
-   int dinner;
-   if (departure<7.00)
-       breakfast=days;
-   else if(departure>7.00)
-breakfast=days-1;
-if(arrival>8.00)
-breakfast=breakfast;
-else if(arrival<8.00)
-       breakfast=breakfast-1;
-   if(departure<12.00)
-       lunch=days;
-   else if(departure>12.00)
-       lunch=days-1;
-   if(arrival>13.00)
-       lunch=lunch;
-   else if(arrival<13.00)
-       lunch=lunch-1;
-   if(departure<18.00)
-       dinner=days;
-   else if(departure>18.00)
-       dinner=days-1;
-   if(arrival>19.00)
-       dinner=dinner;
-   else if(arrival<19.00)
-       dinner=dinner-1;
-   double b,l,d;
-   b=getBreakfast();
-   l=getLunch();
-   d= getDinner();
-   totalExpenses=totalExpenses+(b*breakfast)+(l*lunch)+(d*dinner);
-   return (9*breakfast)+(12*lunch)+(16*dinner);
+    {
+       int breakfast;
+       int lunch;
+       int dinner;
+       if (departure<7.00)
+           breakfast=days;
+       else if(departure>7.00)
+    breakfast=days-1;
+    if(arrival>8.00)
+    breakfast=breakfast;
+    else if(arrival<8.00)
+           breakfast=breakfast-1;
+       if(departure<12.00)
+           lunch=days;
+       else if(departure>12.00)
+           lunch=days-1;
+       if(arrival>13.00)
+           lunch=lunch;
+       else if(arrival<13.00)
+           lunch=lunch-1;
+       if(departure<18.00)
+           dinner=days;
+       else if(departure>18.00)
+           dinner=days-1;
+       if(arrival>19.00)
+           dinner=dinner;
+       else if(arrival<19.00)
+           dinner=dinner-1;
+       double b,l,d;
+       b=getBreakfast();
+       l=getLunch();
+       d= getDinner();
+       totalExpenses=totalExpenses+(b*breakfast)+(l*lunch)+(d*dinner);
+       return (9*breakfast)+(12*lunch)+(16*dinner);
 
-}
+    }
 double getBreakfast()
-{
-   double cost;
-   printf("Enter the cost of breakfast\n");
-   scanf(cost);
-   return cost;
-}
+    {
+       double cost;
+       printf("Enter the cost of breakfast\n");
+       scanf(cost);
+       return cost;
+    }
 double getLunch()
-{
-   double cost;
-   printf("Enter the cost of Lunch\n");
-   scanf(cost);
-   return cost;
-}
+    {
+       double cost;
+       printf("Enter the cost of Lunch\n");
+       scanf(cost);
+       return cost;
+    }
 double getDinner()
-{
-   double cost;
-   printf("Enter the cost of Dinner\n");
-   scanf(cost);
-   return cost;
-}
+    {
+       double cost;
+       printf("Enter the cost of Dinner\n");
+       scanf(cost);
+       return cost;
+    }
